79b87070c3ae52724afc6648eae8fe9259925498
Hamza Ghazouani <hamza.ghazouani@gmail.com>
2016-05-02 09:32:31 +0200

    Fix for storyboard warnings in Xcode 7.3

---
+++ StackView Warnings Xcode 7.3/StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by OnMobile_iOS on 29/04/2016.
+//  Copyright © 2016 Onmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
