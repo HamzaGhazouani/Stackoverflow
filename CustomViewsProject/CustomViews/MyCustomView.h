e1243d36b2329826f5d5c7dd5cbbb47769f3a255
Hamza GHAZOUANI <hamza.ghazouani@onmobile.com>
2016-08-25 10:07:07 +0200

    Connect IBOutlet to class in CustomViews Projects

--- CustomViewsProject/CustomViews/MyCustomView.h
+++ CustomViewsProject/CustomViews/MyCustomView.h
@@ -10,4 +10,6 @@
 
 @interface MyCustomView : CustomViewWithXib
 
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
 @end
--- CustomViewsSwift/CustomViewsSwift/MyCustomView.swift
+++ CustomViewsSwift/CustomViewsSwift/MyCustomView.swift
@@ -10,4 +10,6 @@ import UIKit
 
 class MyCustomView: CustomViewWithXib {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
 }
